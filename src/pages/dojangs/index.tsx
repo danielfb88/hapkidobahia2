73c58add694f7f87a5968ccd8ede0bc8d97831a4
Daniel Bonfim <daniel.fb88@gmail.com>
2023-06-16 10:58:48 -0300

    chore: wip layout dojangs

--- src/pages/dojangs/index.tsx
+++ src/pages/dojangs/index.tsx
@@ -29,87 +29,87 @@ export default function Managers() {
           <div className={styles.associationBox}>
             <div className={styles.associationName}>Centro de Treinamento e Estudo do Hapkido</div>
 
-            <div className={styles.teacherBox}>     
+            <div className={styles.infoContentBox}>     
               <Image
-                  className={styles.teacherPic}
-                  alt="Mestre Luiz Cláudio Xavier"
-                  src={dojangCtehTeachPic}
-                  priority
+                className={styles.teacherPic}
+                alt="Mestre Luiz Cláudio Xavier"
+                src={dojangCtehTeachPic}
+                priority
               />
-            </div>
-                
-                {/* <div className={styles.associationInfoBox}>
-                  <div className={styles.associationAddressBox}>
-                    Mestre:<br />
+                  
+              <div className={styles.associationInfoBox}>
+                <div className={styles.associationAddressBox}>
+                  Mestre:<br />
 
-                      <table width="230px">
-                          <tr>
-                              <td><strong>Luiz Cláudio Xavier</strong></td>
-                              <td>6º Dan</td>
-                          </tr>                                
-                      </table>
-                      <br />
+                    <table width="230px">
+                        <tr>
+                            <td><strong>Luiz Cláudio Xavier</strong></td>
+                            <td>6º Dan</td>
+                        </tr>                                
+                    </table>
+                    <br />
 
-                      Faixas-pretas:<br />
+                    {/* Faixas-pretas:<br />
 
-                      <table width="230px">
-                          <tr>
-                              <td><strong>Carlos Alberto Ted</strong></td>
-                          </tr>
-                          <tr>
-                              <td><strong>Raimunda Oliveira</strong></td>
-                          </tr>
-                          <tr>
-                              <td><strong>Arlesson Sá</strong></td>
-                          </tr>
-                          <tr>
-                              <td><strong>Luiz Carlos Xavier</strong></td>
-                          </tr>
-                          <tr>
-                              <td><strong>Alfredo Xavier</strong></td>
-                          </tr>
-                          <tr>
-                              <td><strong>Edcláudio Xavier</strong></td>
-                          </tr>
-                          <tr>
-                              <td><strong>Carlos A. Damasceno</strong></td>
-                          </tr>
-                          <tr>
-                              <td><strong>Daniel Bonfim</strong></td>
-                          </tr>
-                          <tr>
-                              <td><strong>Paulo Henrique Amorim</strong></td>
-                          </tr>
-                          <tr>
-                              <td><strong>Sandro Ramos</strong></td>
-                          </tr>
-                      </table>
-                      <br />
-                      
-                      R. Fernando de Noronha, 126 - Pituba, Salvador - BA, 41900-385<br />
-                      Salvador - BA<br />
-                      Tel: 55 071 98896-9731<br /><br />
+                    <table width="230px">
+                        <tr>
+                            <td><strong>Carlos Alberto Ted</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Raimunda Oliveira</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Arlesson Sá</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Luiz Carlos Xavier</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Alfredo Xavier</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Edcláudio Xavier</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Carlos A. Damasceno</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Daniel Bonfim</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Paulo Henrique Amorim</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Sandro Ramos</strong></td>
+                        </tr>
+                    </table>
+                    <br />
+                    
+                    R. Fernando de Noronha, 126 - Pituba, Salvador - BA, 41900-385<br />
+                    Salvador - BA<br />
+                    Tel: 55 071 98896-9731<br /><br />
 
-                      Segundas, quartas e sextas às 19:00<br />
-                      Sábados às 10:00 <br /><br />
+                    Segundas, quartas e sextas às 19:00<br />
+                    Sábados às 10:00 <br /><br /> */}
 
-                      <a href="http://www.facebook.com/pages/Centro-De-Treinamento-E-Estudo-Do-Hapkido/587294861438688" target="_blank">
-                          <img src="resource/img/facebook-icon.png" alt="Centro de Treinamento e Estudo do Hapkido" title="Centro de Treinamento e Estudo do Hapkido" />    
-                      </a>
+                    {/* <a href="http://www.facebook.com/pages/Centro-De-Treinamento-E-Estudo-Do-Hapkido/587294861438688" target="_blank">
+                        <img src="resource/img/facebook-icon.png" alt="Centro de Treinamento e Estudo do Hapkido" title="Centro de Treinamento e Estudo do Hapkido" />    
+                    </a>
 
-                      <a href="https://www.instagram.com/mestrexavier/" target="_blank">
-                          <img src="resource/img/instagram-icon.png" alt="Centro de Treinamento e Estudo do Hapkido" title="Centro de Treinamento e Estudo do Hapkido" />
-                      </a>
+                    <a href="https://www.instagram.com/mestrexavier/" target="_blank">
+                        <img src="resource/img/instagram-icon.png" alt="Centro de Treinamento e Estudo do Hapkido" title="Centro de Treinamento e Estudo do Hapkido" />
+                    </a> */}
+                </div>
+
+                {/*  <div className={styles.associationSymbolBox}>
+                      <img src="resource/img/escudo-cteh.png" alt="Centro de Treinamento e Estudo do Hapkido" title="Centro de Treinamento e Estudo do Hapkido" />
                   </div>
 
-                    <div className={styles.associationSymbolBox}>
-                        <img src="resource/img/escudo-cteh.png" alt="Centro de Treinamento e Estudo do Hapkido" title="Centro de Treinamento e Estudo do Hapkido" />
-                    </div>
-
-                    <div className={styles.associationMapBox}>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31099.07613083338!2d-38.48528236044922!3d-13.011168200000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161b6116eac2db%3A0x473ea3af7c4226f9!2sCentro%20de%20Treinamento%20e%20Estudo%20do%20Hapkido%20-%20Academia%20Mestre%20Xavier!5e0!3m2!1spt-BR!2sbr!4v1648909474022!5m2!1spt-BR!2sbr" width="450" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-                    </div>  
-                </div> */}
+                  <div className={styles.associationMapBox}>
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31099.07613083338!2d-38.48528236044922!3d-13.011168200000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161b6116eac2db%3A0x473ea3af7c4226f9!2sCentro%20de%20Treinamento%20e%20Estudo%20do%20Hapkido%20-%20Academia%20Mestre%20Xavier!5e0!3m2!1spt-BR!2sbr!4v1648909474022!5m2!1spt-BR!2sbr" width="450" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                  </div>   */}
+              </div>
+            </div>
           </div>
         
            
--- src/pages/vocabulary/index.tsx
+++ src/pages/vocabulary/index.tsx
@@ -11,159 +11,159 @@ export default function Vocabulary() {
       <main className={styles.content}>
         <div className={styles.title}>Vocabulario</div>
 
-        <table>
-            <tr>
-              <td><strong>Tchariô</strong></td>
-              <td>Atenção</td>
+        <table className={styles.table}>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Tchariô</strong></td>
+              <td className={styles.tdTh}>Atenção</td>
             </tr>
 
-            <tr>
-              <td><strong>Tchumbi</strong></td>
-              <td>Preparar</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Tchumbi</strong></td>
+              <td className={styles.tdTh}>Preparar</td>
             </tr>
 
-            <tr>
-              <td><strong>Bum</strong></td>
-              <td>Tigre</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Bum</strong></td>
+              <td className={styles.tdTh}>Tigre</td>
             </tr>
 
-            <tr>
-              <td><strong>Moo</strong></td>
-              <td>Marcial</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Moo</strong></td>
+              <td className={styles.tdTh}>Marcial</td>
             </tr>
 
-            <tr>
-              <td><strong>Andjá</strong></td>
-              <td>Sentar</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Andjá</strong></td>
+              <td className={styles.tdTh}>Sentar</td>
             </tr>                  
 
-            <tr>
-              <td><strong>Irossôt</strong></td>
-              <td>Levantar</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Irossôt</strong></td>
+              <td className={styles.tdTh}>Levantar</td>
             </tr>
 
-            <tr>
-              <td><strong>Bal Tchagui Tchumbi</strong></td>
-              <td>Preparar para chute</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Bal Tchagui Tchumbi</strong></td>
+              <td className={styles.tdTh}>Preparar para chute</td>
             </tr>
 
-            <tr>
-              <td><strong>Shiô</strong></td>
-              <td>Descansar</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Shiô</strong></td>
+              <td className={styles.tdTh}>Descansar</td>
             </tr>
 
-            <tr>
-              <td><strong>Kyon né</strong></td>
-              <td>Saudação</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Kyon né</strong></td>
+              <td className={styles.tdTh}>Saudação</td>
             </tr>
 
-            <tr>
-              <td><strong>Murop Curô</strong></td>
-              <td>Ajoelhar</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Murop Curô</strong></td>
+              <td className={styles.tdTh}>Ajoelhar</td>
             </tr>
 
-            <tr>
-              <td><strong>Cheoun haru bonaeyo</strong></td>
-              <td>Tenha um bom dia</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Cheoun haru bonaeyo</strong></td>
+              <td className={styles.tdTh}>Tenha um bom dia</td>
             </tr>
 
-            <tr>
-              <td><strong>Heonjeong</strong></td>
-              <td>Dedicação</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Heonjeong</strong></td>
+              <td className={styles.tdTh}>Dedicação</td>
             </tr>
 
-            <tr>
-              <td><strong>Jokio Nim</strong></td>
-              <td>Auxiliar de Professor (3º GUB ao 1º DAN)</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Jokio Nim</strong></td>
+              <td className={styles.tdTh}>Auxiliar de Professor (3º GUB ao 1º DAN)</td>
             </tr>
 
-            <tr>
-              <td><strong>Kiosanim</strong></td>
-              <td>Professor (2º e 3º DAN)</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Kiosanim</strong></td>
+              <td className={styles.tdTh}>Professor (2º e 3º DAN)</td>
             </tr>
 
-            <tr>
-              <td><strong>Sabonim</strong></td>
-              <td>Mestre (4º ao 6º DAN)</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Sabonim</strong></td>
+              <td className={styles.tdTh}>Mestre (4º ao 6º DAN)</td>
             </tr>
 
-            <tr>
-              <td><strong>Kwandjanim</strong></td>
-              <td>Grão-Mestre (7º ao 10º DAN)</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Kwandjanim</strong></td>
+              <td className={styles.tdTh}>Grão-Mestre (7º ao 10º DAN)</td>
             </tr>
 
-            <tr>
-              <td><strong>GUB</strong></td>
-              <td>Grau de faixa colorida</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>GUB</strong></td>
+              <td className={styles.tdTh}>Grau de faixa colorida</td>
             </tr>
-            <tr>
-              <td><strong>DAN</strong></td>
-              <td>Grau de faixa preta</td>
-            </tr>
-
-            <tr>
-              <td><strong>Anion Rasseiô</strong></td>
-              <td>Cumprimento</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>DAN</strong></td>
+              <td className={styles.tdTh}>Grau de faixa preta</td>
             </tr>
 
-            <tr>
-              <td><strong>Gam sá Ramnindá</strong></td>
-              <td>Obrigado</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Anion Rasseiô</strong></td>
+              <td className={styles.tdTh}>Cumprimento</td>
             </tr>
 
-            <tr>
-              <td><strong>Tchonmanevô</strong></td>
-              <td>Disponha</td>
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Gam sá Ramnindá</strong></td>
+              <td className={styles.tdTh}>Obrigado</td>
+            </tr>
+
+            <tr className={styles.tdTh}>
+              <td className={styles.tdTh}><strong>Tchonmanevô</strong></td>
+              <td className={styles.tdTh}>Disponha</td>
             </tr>
           </table>
 
           <br />
 
         <div className={styles.title}>Quedas</div>
-        <table>
-          <tr>
-            <td><strong>Ruégion Napop</strong></td>
-            <td>Rolamento</td>
+        <table className={styles.table}>
+          <tr className={styles.tdTh}>
+            <td className={styles.tdTh}><strong>Ruégion Napop</strong></td>
+            <td className={styles.tdTh}>Rolamento</td>
           </tr>
 
-          <tr>
-            <td><strong>Napop</strong></td>
-            <td>Queda com 'brake'</td>
+          <tr className={styles.tdTh}>
+            <td className={styles.tdTh}><strong>Napop</strong></td>
+            <td className={styles.tdTh}>Queda com 'brake'</td>
           </tr>
 
-          <tr>
-            <td><strong>Tchambam Napop</strong></td>
-            <td>Queda de frente</td>
+          <tr className={styles.tdTh}>
+            <td className={styles.tdTh}><strong>Tchambam Napop</strong></td>
+            <td className={styles.tdTh}>Queda de frente</td>
           </tr>
 
-          <tr>
-            <td><strong>Fuban Napop</strong></td>
-            <td>Queda de costas</td>
+          <tr className={styles.tdTh}>
+            <td className={styles.tdTh}><strong>Fuban Napop</strong></td>
+            <td className={styles.tdTh}>Queda de costas</td>
           </tr>
 
-          <tr>
-            <td><strong>Tchukbang Napop</strong></td>
-            <td>Queda lateral</td>
+          <tr className={styles.tdTh}>
+            <td className={styles.tdTh}><strong>Tchukbang Napop</strong></td>
+            <td className={styles.tdTh}>Queda lateral</td>
           </tr>
 
-          <tr>
-            <td><strong>Kondiun Napop</strong></td>
-            <td>Queda aérea sem as mãos</td>
+          <tr className={styles.tdTh}>
+            <td className={styles.tdTh}><strong>Kondiun Napop</strong></td>
+            <td className={styles.tdTh}>Queda aérea sem as mãos</td>
           </tr>
 
-          <tr>
-            <td><strong>Koien Napop</strong></td>
-            <td>Salto sobre obstáculo</td>
+          <tr className={styles.tdTh}>
+            <td className={styles.tdTh}><strong>Koien Napop</strong></td>
+            <td className={styles.tdTh}>Salto sobre obstáculo</td>
           </tr>
 
-          <tr>
-            <td><strong>Tchaien Mul Napop</strong></td>
-            <td>Salto à distância com rolamento</td>
+          <tr className={styles.tdTh}>
+            <td className={styles.tdTh}><strong>Tchaien Mul Napop</strong></td>
+            <td className={styles.tdTh}>Salto à distância com rolamento</td>
           </tr>
 
-          <tr>
-            <td><strong>Tchambrim napop</strong></td>
-            <td>Queda com salto mortal</td>
+          <tr className={styles.tdTh}>
+            <td className={styles.tdTh}><strong>Tchambrim napop</strong></td>
+            <td className={styles.tdTh}>Queda com salto mortal</td>
           </tr>
         </table>  
           
